806aa486b38ec06f5d3093ebed640321b9f29623
idesiegramos <159062617+idesiegramos@users.noreply.github.com>
2024-08-28 19:11:51 +0200

    Add files via upload

---
+++ acerca_de.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Acerca de")
+st.write("Información legal")
+st.write("Lorem ipsum odor amet, consectetuer adipiscing elit. Elementum dignissim neque suscipit nec montes mus ridiculus libero.")
---
+++ inicio.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+
+
+st.image("OIP.png")
+
+st.title("IDE-IA")
+st.write("Bienvenido a la Inteligencia Artificial de IDESIE")
+st.write("Busca, investiga, documenta. Aquí, donde la tecnología de la Inteligencia Aritficial se une al aprendizaje avanzado, encontrarás todo lo que necesites.")
+st.write("A continuación te mostramos las principales utilidades:")
+st.write("1.-Transcribe de manera automática y precisa los videos de tus clases del Máster BIM.")
+st.write("2.- Resume el contenido principal de las clases.")
+st.write("3.- Responde preguntas relacionadas con el vídeo de las clases.")
+st.page_link("ia.py", label="Comenzar", icon="➡️")
