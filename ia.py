11bbcfb0f3745cc2057187ac4647f52548c31e19
idesiegramos <159062617+idesiegramos@users.noreply.github.com>
2024-09-02 21:27:25 +0200

    Update ia.py

--- ia.py
+++ ia.py
@@ -192,19 +192,18 @@ if prompt := st.chat_input("Escribe tu pregunta"):
     with st.chat_message("user"):
         st.markdown(prompt)
 
-
-# Mostrar la respuesta del asistente en el contenedor de chat
-with st.chat_message("assistant"):
-    stream = client.chat.completions.create(
-        model=st.session_state["openai_model"],
-        messages=[
-            {"role": m["role"], "content": m["content"]}
-            for m in st.session_state.messages
-        ],
-        stream=True,
-    )
-    response = st.write_stream(stream)
-st.session_state.messages.append({"role": "assistant", "content": response})
+    # Mostrar la respuesta del asistente en el contenedor de chat
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
 
 
 #
