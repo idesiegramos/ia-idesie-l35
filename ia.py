b47334b1aa122764031c3e02201f8570d09f8944
leirealvira <159037822+leirealvira@users.noreply.github.com>
2024-10-03 20:53:59 +0200

    Update ia.py

--- ia.py
+++ ia.py
@@ -194,7 +194,24 @@ documents = [
 #    embedding=OpenAIEmbeddings(),
 #)
 
+######################
+# CHROMA + LANGCHAIN
+######################
 
+import getpass
+
+    os.environ["OPENAI_API_KEY"] = getpass.getpass()
+
+from langchain_openai import OpenAIEmbeddings
+
+embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+
+from langchain_chroma import Chroma
+
+vector_store = Chroma(
+    collection_name="example_collection",
+    embedding_function=embeddings,
+    persist_directory="./chroma_langchain_db",  # Where to save data locally, remove if not necessary
 
 
 
